d9ce48843527572f219a3a86c37f3526332bbe16
Herczku Misi <kmisku@gmail.com>
2016-04-02 15:49:31 +0200

    add Create Class files

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CreateClass
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthDate;
+
+        public Person(String Name, DateTime birthDate)
+        {
+            this.Name = Name;
+            this.birthDate = birthDate;
+        }
+
+        public enum Genders : int
+        {
+            Male, Female
+        };
+        public Genders gender;
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("name:{0}, birth date{1}", this.name, this.birthDate);
+        }
+    }
+}
